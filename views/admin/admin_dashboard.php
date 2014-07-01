1011144619f0592dfb11e7f3c3853c7063634dfc
apurvamrutkar <pmamrutkar@gmail.com>
2014-07-01 10:14:22 +0530

    Merge origin/master changes in admin_dashboard.php for Panels

--- views/admin/admin_dashboard.php
+++ views/admin/admin_dashboard.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Hexagraph Analytics</title>
+        <title>Hexagraph Analytics</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../assets/css/bootstrap.css" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="../../assets/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Add custom CSS here -->
-    <link href="../../assets/css/sidebar.css" rel="stylesheet">
+        <!-- Add custom CSS here -->
+        <link href="../../assets/css/sidebar.css" rel="stylesheet">
+        <!-- Custon panel color settings-->
 
-</head>
-<body>
- <!-- Navbar code goes here -->
-   <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class =" container">
-        <a href="admin_dashboard.php" class =" navbar-brand">HexaGraph</a>
+    </head>
+    <body>
+        <!-- Navbar code goes here -->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class =" container">
+                <a href="admin_dashboard.php" class =" navbar-brand">HexaGraph</a>
 
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="overview.php">Home</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">UserName<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="edit_profile.php">Edit profile</a></li>
-                        <li><a href="../../model/logout.php">Logout</a></li>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="overview.php">Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">UserName<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="edit_profile.php">Edit profile</a></li>
+                                <li><a href="../../model/logout.php">Logout</a></li>
 
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<!-- ***************************SIDE PANEL GOES HERE*******************-->
-    <div class="container">
-        <div class="row">
-             <div class="col-lg-2 sidebar">
+        <!-- ***************************SIDE PANEL GOES HERE*******************-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 sidebar">
                     <!-- Overview  -->
                     <ul class="nav nav-sidebar">
                         <li class="active" ><a id="overview" href="overview.php">Overview</a></li>
                     </ul>
-                    
+
                     <ul class="nav nav-sidebar">
                         <li><a href="manage_class.php">Manage class</a></li>
                     </ul>
@@ -54,14 +55,55 @@
                     <ul class="nav nav-sidebar">
                         <li><a href="manage_exam.php">Manage Exam</a></li>
                     </ul>
-                    
+
                 </div>
-            
-            
+
+                <!--******************Dashboard content goes here*************-->
+                <div class="col-sm-8 col-sm-offset-1 main" >             
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <div class="panel panel-info">
+                                    <div class="panel-heading">
+                                        
+                                        <div class="row">
+                                           
+                                            <div class="col-xs-6 text-left">
+                                                
+                                                <span class="glyphicon glyphicon-book" style="font-size: 40px">
+                                                </span>
+                                                    
+                                            </div>
+                                            <div class="col-xs-6 text-left" style="font-family: cursive; font-size: x-large; color: #285e8e">
+                                                Class  1
+                                            </div>
+                                            <!--<p style="font-family: cursive; font-size: x-large; color: #285e8e">-->
+                                        </div>
+                                        
+                                    </div>
+                                    <a href="#">
+                                        <div class="panel-footer announcement-bottom">
+                                            <div class="row">
+                                                <div class="col-xs-6">
+                                                    Class Details
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+
+
         <hr>
-             <!-- Footer code goes here   -->
+        <!-- Footer code goes here   -->
         <footer>
             <div class="row">
                 <div class="col-lg-12">
@@ -76,8 +118,8 @@
     <!-- JavaScript -->
     <script src="../../assets/js/jquery-1.10.2.js"></script>
     <script src="../../assets/js/bootstrap.js"></script>
+    
 
-  
 </body>
 
 </html>
